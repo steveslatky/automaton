f3992e32fee18b217fa026c5475b08c5d51651bd
Stephen Slatky <steve.slatky@gmail.com>
2018-04-06 10:32:45 -0400

    more pushdown thoughts

--- push_down.py
+++ push_down.py
@@ -1,16 +1,30 @@
+from collections import deque
+
+
 class push_down:
 
-    def __init__(self, Q = None, Sigma = None, Gamma = None, delta = None, q0 = None, Z = None, F = None ):
-        self.Q = Q
-        self.Sigma = Sigma
-        self.Gamma = Gamma
-        self.delta = delta
-        self.q0 = q0
-        self.Z = Z
-        self.F = F
+    def accept(self, delta, q0, F, stack, input):
+        state = q0
+        for c in input:
+            state = delta[state][c]
+            if state[1] is not 'e':
+                stack.append(state[1])
+            elif state[1] is 'e':
+                stack.pop()
+
+        return state in F
 
 
-    def accept(self):
-        pass
+if __name__ == '__main__':
+    z = push_down()
+    s = deque()
 
-# Could I Make an abstact class of automata
+    # dfa = {0: {'0': 0, '1': 1},
+    #        1: {'0': 2, '1': 0},
+    #        2: {'0': 1, '1': 2}}
+
+    # L = {0^n1^n | n >= 0}
+    delta = {0: {'e': (1('e', 'z'))},
+             1: {'0': (1.('e', '0')), '1': (2, ('0', 'e'))},
+             2: {'1': (2, ('0', 'e')), 'e': (3, ('z', 'e'))},
+             3: {}}
