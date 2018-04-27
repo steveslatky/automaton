3b9f47044ff92a13176737813dd3211fe68ece50
Stephen Slatky <steve.slatky@gmail.com>
2018-04-27 19:40:10 -0400

    Feat: Step 2 complete

--- cfa.py
+++ cfa.py
@@ -3,20 +3,37 @@
 import csv
 
 
+class transition:
+
+    def __init__(self, val=None, tmp=None):
+        self.isTmp = tmp
+        self.val = val
+
+
+
 class cfa:
 
     def __init__(self):
-        q = None
-        in_alpha = None
-        out_alpha = None
-        trans_fun = None
-        lam = None
-        initial_state = None
-        rewards = None
-        punish = []
-        confidence = None
-        expect = None
+        self.q = None
+        self.in_alpha = None
+        self.out_alpha = None
+        self.delta = None
+        self.lam = None
+        self.initial_state = None
+        self.rewards = None
+        self.punish = []
+        self.confidence = None
+        self.expect = None
 
+        self.q_curr = 0
+        self.q_last = 0
+        self.a_last = 'e'
+        self.o = 'e'
+        self.o_last = 'e'
+
+    # Get data
+    # Set alphabet
+    # Return: Steps
     @property
     def setup(self):
         f = open("data/test.csv", newline='')
@@ -33,9 +50,36 @@ class cfa:
                 done = True
         return steps
 
+    def run(self):
+        # Abstraction of time for the use of simulation.
+        time = 0
+        steps = self.setup
+        gamma = 15  # TODO I don't know what this should equal at start
+
+        for s in steps:
+            s_time = s[0]
+
+            # STEP 2
+            if (int(s_time) - time) > gamma:
+                tt = transition((self.q_curr, 'e'), True)
+                if tt in self.delta[self.q_curr]:
+                    self.delta[self.q_curr][self.delta[self.q_curr].index(tt)].isTmp = False
+                self.q_last = self.q_curr
+                self.q_curr = tt
+
+            q_anchor = self.q_curr
+            self.a_last = 'e'
+            self.o_last = 'e'
+
+            # TODO Unmark All symbols b and distributions p ^ delta _ q,a
+
+            # input("Press enter to continue")
+
+
+def main():
+    t = cfa()
+    t.run()
+
 
 if __name__ == "__main__":
-    t = cfa()
-    print(t.setup)
-    print(t.in_alpha)
-    print(t.out_alpha)
+    main()
