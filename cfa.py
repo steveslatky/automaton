e608b9921a2430cbe43129fd9ffea7c4e6a8c05b
Stephen Slatky <steve.slatky@gmail.com>
2018-04-27 18:20:47 -0400

    Feat: Fixed dataset, setup working

--- cfa.py
+++ cfa.py
@@ -1,12 +1,14 @@
 #!./mac_env/bin/python3
 
+import csv
+
 
 class cfa:
 
     def __init__(self):
         q = None
-        in_alpha = []
-        out_alpha = []
+        in_alpha = None
+        out_alpha = None
         trans_fun = None
         lam = None
         initial_state = None
@@ -15,30 +17,25 @@ class cfa:
         confidence = None
         expect = None
 
-
-
+    @property
     def setup(self):
-        f = open("../data/test.csv")
+        f = open("data/test.csv", newline='')
         steps = []
-        done = False
-        for row in f:
-            if row[0:2] == "in":
-                tmp_alpha = row[3:].strip("\n").lstrip(' ')
-                self.in_alpha = tmp_alpha.split(',')
-            if row[0:3] == "out":
-                tmp_alpha = row[4:].strip("\n").lstrip(" ")
-                self.out_alpha = tmp_alpha.split(',')
-                done = True
+        done = False  # To help get the steps when it gets past the header
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
             if done:
                 steps.append(row)
+            if row[0] == "in":
+                self.in_alpha = row[1:]
+            if row[0] == "out":
+                self.out_alpha = row[1:]
+                done = True
         return steps
-
-
 
 
 if __name__ == "__main__":
     t = cfa()
-    print(t.setup())
-
-
-
+    print(t.setup)
+    print(t.in_alpha)
+    print(t.out_alpha)
