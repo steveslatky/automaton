d9acde220023b0c1e8a63628b1e43060f4d565fe
Stephen Slatky <steve.slatky@gmail.com>
2018-06-10 19:42:10 -0400

    Version 3 Song Generator

--- text_generator/markov_chain.py
+++ text_generator/markov_chain.py
@@ -4,6 +4,7 @@
 # A simple text generator using a markov chain.
 # It will take in a text source and output works based
 # on the source.
+from os import listdir
 from random import randint
 
 
@@ -41,10 +42,8 @@ class chain:
                         pass
 
     def create_song(self, line_len=8, song_len=60):
-        line_count = 0
         song = []
         for line_count in range(song_len):
-            words_in_line = 0
             word = None
             line = []
             for words_in_line in range(line_len):
@@ -52,14 +51,17 @@ class chain:
                 if word is None:
                     word = list(self.alpha.keys())[randint(0, len(self.alpha) - 1)]
                 else:
-                    # get strongest link from word
                     try:
                         next_words_list = self.alpha[word]
                         word = pick_word(next_words_list)
-                    except KeyError:
+                    except KeyError:  # TODO Fix what happens if you don't find connecting word
                         break
                 line.append(word)
             song.append(line)
+            # 1/10 times it will duplicate a line to make a chorus
+            while randint(0,10) is 10:
+                song.append(line)
+
         write_song(song)
 
 
@@ -69,25 +71,50 @@ class chain:
 # Takes in a list of lines where each line
 # has a list of words in them
 def write_song(song):
-    with open("./songs/1.txt", 'w') as f:
+    song_num = len(listdir("./songs")) + 1
+    with open("./songs/" + str(song_num) + ".txt", 'w') as f:
+        description = "==========================================================\n" \
+                      "Version 3, Playing around with the idea of a chorus \n" \
+                      "It just writes a line over again with 1/10 probability\n" \
+                      "==========================================================\n"
+        f.write(description)
         for line in song:
             if line is not None:
-                f.write(" ".join(line))
-                f.write("\n")
+                f.write(" ".join(line) + "\n")
 
 
 def pick_word(next_word_list):
+    # Get list of words and how many times they are seen.
     combo_list = []
     for word in next_word_list:
         combo_list.append((next_word_list[word].num_seen, word))
-    combo_list = sorted(combo_list)
-    return combo_list[len(combo_list) - 1][1]
+    combo_list = sorted(combo_list, reverse=True)
+
+    # Count total amount of times all words were seen and then
+    # Roll the dice to see what word to pick
+    total = 0
+    for word in combo_list:
+        total += word[0]
+    dice_roll = randint(0, total - 1)
+
+    # If the list is 1 just return that word
+    count = 0
+    if len(combo_list) is 1:
+        return combo_list[0][1]
+
+    # Get word based on probability that it showed up.
+    for word in combo_list:
+        if count >= dice_roll:
+            return word[1]
+        else:
+            count += word[0]
 
 
 def main():
+    source = "./sources/kanye/"
     generator = chain()
-    for i in range(1, 8):
-        generator.read_source("./sources/kanye/" + str(i) + ".txt")
+    for i in range(1, len(listdir(source)) + 1):
+        generator.read_source(source + str(i) + ".txt")
     generator.create_song()
 
 
